29e10ec551c353c59cdd1a89c29835638d19c3cd
Alice Antunes <aliceoliveiraaa7995@gmail.com>
2026-09-10 13:19:58 -0300

    Correção do código

--- desconto.js
+++ desconto.js
@@ -8,14 +8,12 @@ btnCalcular.addEventListener("click", function () {
   // CONVERTE TEXT PARA NUMBER
   let ValorCompra = Number(inputValorCompra.value);
 
-  let valor = ValorCompra * 0.9;
-
-
   if (ValorCompra >= 200) {
     let valorFinal = ValorCompra * 0.9;
     let desconto = ValorCompra * 0.1;
-    pResultado.innerText = "Desconto APROVADO! Desconto: " + desconto;
-    pResultado.innerText = "Valor com desconto: " + valorFinal;
+    
+    // Junta as mensagens com \n para quebrar a linha
+    pResultado.innerText = "Desconto: R$" + desconto + " Valor com desconto: R$" + valorFinal;
     pResultado.style.color = "green";
   } else {
     pResultado.innerText = "Desconto NÃO APROVADO!";
